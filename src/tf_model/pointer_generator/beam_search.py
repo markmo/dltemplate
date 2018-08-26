Fixes to Policy Gradients Pong and Summarization with Pointer-Generator Networks examples

src/tf_model/pointer_generator/beam_search.py
@@ -1,10 +1,6 @@
 """ This file contains code to run beam search decoding """
-import tf_model.pointer_generator.data as data
 import numpy as np
-import tensorflow as tf
-
-
-FLAGS = tf.app.flags.FLAGS
+from tf_model.pointer_generator.data import START_DECODING, STOP_DECODING, UNKNOWN_TOKEN
 
 
 class Hypothesis(object):
@@ -69,7 +65,7 @@ class Hypothesis(object):
         return self.log_prob / len(self.tokens)
 
 
-def run_beam_search(sess, model, vocab, batch):
+def run_beam_search(sess, model, vocab, batch, min_dec_steps, max_dec_steps, beam_size):
     """
     Performs beam search decoding on the given example.
 
@@ -77,6 +73,9 @@ def run_beam_search(sess, model, vocab, batch):
     :param model: a seq2seq model
     :param vocab: Vocabulary object
     :param batch: Batch object that is the same example repeated across the batch
+    :param min_dec_steps:
+    :param max_dec_steps:
+    :param beam_size:
     :return:
         best_hyp: Hypothesis object; the best hypothesis found by beam search.
     """
@@ -86,20 +85,20 @@ def run_beam_search(sess, model, vocab, batch):
     # enc_states has shape [batch_size, <=max_enc_steps, 2*hidden_dim].
 
     # Initialize hypotheses (beam_size)
-    hyps = [Hypothesis(tokens=[vocab.word2id(data.START_DECODING)],
+    hyps = [Hypothesis(tokens=[vocab.word2id(START_DECODING)],
                        log_probs=[0.],
                        state=dec_in_state,
                        attn_dists=[],
                        p_gens=[],
                        coverage=np.zeros([batch.enc_shape[1]])  # zero vector of length attention_length
-                       ) for _ in range(FLAGS.beam_size)]
+                       ) for _ in range(beam_size)]
     results = []  # this will contain finished hypotheses (those that have emitted the [STOP] token)
     steps = 0
-    while steps < FLAGS.max_dec_steps and len(results) < FLAGS.beam_size:
+    while steps < max_dec_steps and len(results) < beam_size:
         latest_tokens = [h.latest_token for h in hyps]  # latest token produced by each hypothesis
 
         # change any in-article temporary OOV ids to [UNK] id, so that we can lookup word embeddings
-        latest_tokens = [t if t in range(vocab.size()) else vocab.word2id(data.UNKNOWN_TOKEN)
+        latest_tokens = [t if t in range(vocab.size()) else vocab.word2id(UNKNOWN_TOKEN)
                          for t in latest_tokens]
         states = [h.state for h in hyps]  # list of current decoder states of the hypotheses
         prev_coverage = [h.coverage for h in hyps]  # list of coverage vectors (or None)
@@ -125,7 +124,7 @@ def run_beam_search(sess, model, vocab, batch):
             new_coverage_i = new_coverage[i]
 
             # for each of the top 2 * beam_size hypotheses
-            for j in range(FLAGS.beam_size * 2):
+            for j in range(beam_size * 2):
                 # extend the ith hypothesis with the jth option
                 new_hyp = h.extend(token=top_k_ids[i, j],
                                    log_prob=top_k_log_probs[i, j],
@@ -138,14 +137,14 @@ def run_beam_search(sess, model, vocab, batch):
         # Filter and collect any hypotheses that have produced the end token.
         hyps = []  # will contain hypotheses for the next step
         for h in sort_hyps(all_hyps):  # in order of most likely hypothesis
-            if h.latest_token == vocab.word2id(data.STOP_DECODING):  # if stop token is reached...
+            if h.latest_token == vocab.word2id(STOP_DECODING):  # if stop token is reached...
                 # If this hypothesis is sufficiently long, put in results. Otherwise discard.
-                if steps >= FLAGS.min_dec_steps:
+                if steps >= min_dec_steps:
                     results.append(h)
             else:  # hasn't reached stop token, so continue to extend this hypothesis
                 hyps.append(h)
 
-            if len(hyps) == FLAGS.bean_size or len(results) == FLAGS.beam_size:
+            if len(hyps) == beam_size or len(results) == beam_size:
                 # Once we've collected beam_size-many hypotheses for the next step,
                 # or beam_size-many complete hypotheses, stop.
                 break

src/tf_model/pointer_generator/decode.py
@@ -3,13 +3,10 @@ import logging
 import os
 import pyrouge
 import tensorflow as tf
-import tf_model.pointer_generator.beam_search as beam_search
-import tf_model.pointer_generator.data as data
-import tf_model.pointer_generator.util as util
+from tf_model.pointer_generator.beam_search import run_beam_search
+from tf_model.pointer_generator.data import output_ids2words, show_abs_oovs, show_art_oovs, STOP_DECODING
+from tf_model.pointer_generator.util import get_config, load_ckpt
 import time
-
-
-FLAGS = tf.app.flags.FLAGS
 
 
 SECS_UNTIL_NEW_CKPT = 60  # max number of seconds before loading new checkpoint
@@ -18,38 +15,50 @@ SECS_UNTIL_NEW_CKPT = 60  # max number of seconds before loading new checkpoint
 # noinspection SpellCheckingInspection
 class BeamSearchDecoder(object):
 
-    def __init__(self, model, batcher, vocab):
+    def __init__(self, model, batcher, vocab, constants):
         """
 
         :param model: Seq2SeqAttentionModel object
         :param batcher: Batcher object
         :param vocab: Vocabulary object
+        :param constants:
         """
         self._model = model
         model.build_graph()
         self._batcher = batcher
         self._vocab = vocab
+        self._min_dec_steps = constants['min_dec_steps']
+        self._max_dec_steps = constants['max_dec_steps']
+        self._beam_size = constants['beam_size']
+        self._single_pass = constants['single_pass']
+        self._pointer_gen = constants['pointer_gen']
+        self._log_root = constants['log_root']
         self._saver = tf.train.Saver()  # we use this to load checkpoints for decoding
-        self._sess = tf.Session(config=util.get_config())
+        self._sess = tf.Session(config=get_config())
 
         # Load an initial checkpoint to use for decoding
-        ckpt_path = util.load_ckpt(self._saver, self._sess)
+        ckpt_path = load_ckpt(self._saver, self._sess, self._log_root)
 
-        if FLAGS.single_pass:
+        if self._single_pass:
             # Make a descriptive decode directory name
             ckpt_name = 'ckpt-' + ckpt_path.split('-')[-1]  # this is something of the form "ckpt-123456"
-            self._decode_dir = os.path.join(FLAGS.log_root, get_decode_dir_name(ckpt_name))
+            self._decode_dir = os.path.join(self._log_root, get_decode_dir_name(ckpt_name,
+                                                                                constants['data_path'],
+                                                                                constants['max_enc_steps'],
+                                                                                self._min_dec_steps,
+                                                                                self._max_dec_steps,
+                                                                                self._beam_size))
             if os.path.exists(self._decode_dir):
                 raise Exception('single_pass decode directory %s should not already exist' % self._decode_dir)
 
         else:  # Generic decode dir name
-            self._decode_dir = os.path.join(FLAGS.log_root, 'decode')
+            self._decode_dir = os.path.join(constants['log_root'], 'decode')
 
         # Make the decode dir if necessary
         if not os.path.exists(self._decode_dir):
             os.mkdir(self._decode_dir)
 
-        if FLAGS.single_pass:
+        if self._single_pass:
             # Make the dirs to contain output written in the correct format for pyrouge
             self._rouge_ref_dir = os.path.join(self._decode_dir, 'reference')
             if not os.path.exists(self._rouge_ref_dir):
@@ -71,7 +80,7 @@ class BeamSearchDecoder(object):
         while True:
             batch = self._batcher.next_batch()  # one example repeated across batch
             if batch is None:  # finished decoding dataset in single_pass mode
-                assert FLAGS.single_pass, 'Dataset exhausted, but we are not in single_pass mode'
+                assert self._single_pass, 'Dataset exhausted, but we are not in single_pass mode'
                 tf.logging.info('Decoder has finished reading dataset for single_pass.')
                 tf.logging.info('Output has been saved in %s and %s. Now starting ROUGE eval...',
                                 self._rouge_ref_dir, self._rouge_dec_dir)
@@ -83,28 +92,29 @@ class BeamSearchDecoder(object):
             original_abstract = batch.original_abstracts[0]  # string
             original_abstract_sents = batch.original_abstracts_sents[0]  # list of strings
 
-            article_with_unks = data.show_art_oovs(original_article, self._vocab)  # string
-            abstract_with_unks = data.show_abs_oovs(original_abstract, self._vocab,
-                                                    (batch.art_oovs[0] if FLAGS.pointer_gen else None))  # string
+            article_with_unks = show_art_oovs(original_article, self._vocab)  # string
+            abstract_with_unks = show_abs_oovs(original_abstract, self._vocab,
+                                               (batch.art_oovs[0] if self._pointer_gen else None))  # string
 
             # Run beam search to get best Hypothesis
-            best_hyp = beam_search.run_beam_search(self._sess, self._model, self._vocab, batch)
+            best_hyp = run_beam_search(self._sess, self._model, self._vocab, batch,
+                                       self._min_dec_steps, self._max_dec_steps, self._beam_size)
 
             # Extract the output ids from the hypothesis and convert back to words
             output_ids = [int(t) for t in best_hyp.tokens[1:]]
-            decoded_words = data.output_ids2words(output_ids, self._vocab,
-                                                  (batch.art_oovs[0] if FLAGS.pointer_gen else None))
+            decoded_words = output_ids2words(output_ids, self._vocab,
+                                             (batch.art_oovs[0] if self._pointer_gen else None))
 
             # Remove the [STOP] token from decoded_words, if necessary
             try:
-                fst_stop_idx = decoded_words.index(data.STOP_DECODING)  # index of the (first) [STOP] symbol
+                fst_stop_idx = decoded_words.index(STOP_DECODING)  # index of the (first) [STOP] symbol
                 decoded_words = decoded_words[:fst_stop_idx]
             except ValueError:
                 decoded_words = decoded_words
 
             decoded_output = ' '.join(decoded_words)  # string
 
-            if FLAGS.single_pass:
+            if self._single_pass:
                 # write ref summary and decoded summary to file, to eval with pyrouge later
                 self.write_for_rouge(original_abstract_sents, decoded_words, counter)
                 counter += 1  # this is how many examples we've decoded
@@ -121,7 +131,7 @@ class BeamSearchDecoder(object):
                 if t1 - t0 > SECS_UNTIL_NEW_CKPT:
                     tf.logging.info("We've been decoding with same checkpoint for %i seconds. " +
                                     "Time to load new checkpoint", t1 - t0)
-                    _ = util.load_ckpt(self._saver, self._sess)
+                    _ = load_ckpt(self._saver, self._sess, self._log_root)
                     t0 = time.time()
 
     def write_for_rouge(self, reference_sents, decoded_words, ex_index):
@@ -188,7 +198,7 @@ class BeamSearchDecoder(object):
             'abstract_str': make_html_safe(abstract),
             'attn_dists': attn_dists
         }
-        if FLAGS.pointer_gen:
+        if self._pointer_gen:
             to_write['p_gens'] = p_gens
 
         output_filename = os.path.join(self._decode_dir, 'attn_vis_data.json')
@@ -253,25 +263,30 @@ def rouge_log(results_dict, dir_to_write):
         f.write(log_str)
 
 
-def get_decode_dir_name(ckpt_name):
+def get_decode_dir_name(ckpt_name, data_path, max_enc_steps, min_dec_steps, max_dec_steps, beam_size):
     """
     Make a descriptive name for the decode dir, including the name of the checkpoint
     we use to decode. This is called in single_pass mode.
 
     :param ckpt_name:
+    :param data_path:
+    :param max_enc_steps:
+    :param min_dec_steps:
+    :param max_dec_steps:
+    :param beam_size:
     :return:
     """
-    if 'train' in FLAGS.data_path:
+    if 'train' in data_path:
         dataset = 'train'
-    elif 'val' in FLAGS.data_path:
+    elif 'val' in data_path:
         dataset = 'val'
-    elif 'test' in FLAGS.data_path:
+    elif 'test' in data_path:
         dataset = 'test'
     else:
-        raise ValueError('FLAGS.data_path %s should contain one of train, val or test' % FLAGS.data_path)
+        raise ValueError('FLAGS.data_path %s should contain one of train, val or test' % data_path)
     # noinspection SpellCheckingInspection
     dir_name = 'decode_%s_%imaxenc_%ibeam_%imindec_%imaxdec' % \
-               (dataset, FLAGS.max_enc_steps, FLAGS.beam_size, FLAGS.min_dec_steps, FLAGS.max_dec_steps)
+               (dataset, max_enc_steps, beam_size, min_dec_steps, max_dec_steps)
     if ckpt_name is not None:
         dir_name += '_%s' % ckpt_name
 

src/tf_model/pointer_generator/model_setup.py
@@ -1,11 +1,10 @@
 import numpy as np
 import os
+import sys
 import tensorflow as tf
 from tensorflow.contrib.tensorboard.plugins import projector
 from tensorflow.python.ops import array_ops, math_ops, nn_ops, variable_scope
 import time
-
-FLAGS = tf.app.flags.FLAGS
 
 
 # noinspection PyPep8Naming,PyUnusedLocal
@@ -114,7 +113,7 @@ def attention_decoder(decoder_inputs, initial_state, encoder_states, enc_padding
 
                 # Calculate v^T tanh(W_h h_i + W_s s_t + w_c c_i^t + b_attn)
                 # shape (batch_size, attn_length)
-                e = math_ops.reduce_sum(v * math_ops.tan(encoder_features + decoder_features + coverage_features),
+                e = math_ops.reduce_sum(v * math_ops.tanh(encoder_features + decoder_features + coverage_features),
                                         [2, 3])
 
                 # Calculate attention distribution
@@ -135,7 +134,7 @@ def attention_decoder(decoder_inputs, initial_state, encoder_states, enc_padding
             # Calculate the context vector from attn_dist and encoder_states
             # shape (batch_size, attn_size)
             context_vector_ = math_ops.reduce_sum(array_ops.reshape(attn_dist, [batch_size, -1, 1, 1]) *
-                                                  encoder_features, [1, 2])
+                                                  encoder_states, [1, 2])
             context_vector_ = array_ops.reshape(context_vector_, [-1, attn_size])
 
         return context_vector_, attn_dist, coverage_
@@ -156,7 +155,7 @@ def attention_decoder(decoder_inputs, initial_state, encoder_states, enc_padding
         context_vector, _, coverage = attention(initial_state, coverage)
 
     for i, inp in enumerate(decoder_inputs):
-        tf.logging.info('Adding attention_decoder timestep %i of %i', i, len(decoder_inputs))
+        tf.logging.info('Adding attention_decoder timestep %i of %i', i + 1, len(decoder_inputs))
         if i > 0:
             variable_scope.get_variable_scope().reuse_variables()
 
@@ -268,7 +267,7 @@ class SummarizationModel(object):
         self._enc_batch = tf.placeholder(tf.int32, [config.batch_size, None], name='enc_batch')
         self._enc_lens = tf.placeholder(tf.int32, [config.batch_size], name='enc_lens')
         self._enc_padding_mask = tf.placeholder(tf.float32, [config.batch_size, None], name='enc_padding_mask')
-        if FLAGS.pointer_gen:
+        if config.pointer_gen:
             self._enc_batch_extend_vocab = tf.placeholder(tf.int32, [config.batch_size, None],
                                                           name='enc_batch_extend_vocab')
             self._max_art_oovs = tf.placeholder(tf.int32, [], name='max_art_oovs')
@@ -294,7 +293,7 @@ class SummarizationModel(object):
             self._enc_lens: batch.enc_lens,
             self._enc_padding_mask: batch.enc_padding_mask
         }
-        if FLAGS.pointer_gen:
+        if self._config.pointer_gen:
             feed_dict[self._enc_batch_extend_vocab] = batch.enc_batch_extend_vocab
             feed_dict[self._max_art_oovs] = batch.max_art_oovs
 
@@ -447,6 +446,17 @@ class SummarizationModel(object):
             final_dists = [vocab_dist + copy_dist
                            for vocab_dist, copy_dist in zip(vocab_dists_extended, attn_dists_projected)]
 
+            # OOV part of vocab is max_art_oov long. Not all the sequences in a batch
+            # will have max_art_oov tokens. This will cause some entries to be 0 in
+            # the distribution, which results in a NaN when calculating log_dists.
+            # Clip the dist or add epsilon when that happens.
+            # def add_epsilon(dist, epsilon=sys.float_info.epsilon):
+            #     epsilon_mask = tf.ones_like(dist) * epsilon
+            #     return dist + epsilon_mask
+            #
+            # final_dists = [add_epsilon(dist) for dist in final_dists]
+            final_dists = [tf.clip_by_value(dist, sys.float_info.epsilon, 1.) for dist in final_dists]
+
             return final_dists
 
     def _add_emb_vis(self, embedding_var):
@@ -460,7 +470,7 @@ class SummarizationModel(object):
         :param embedding_var:
         :return:
         """
-        train_dir = os.path.join(FLAGS.log_root, 'train')
+        train_dir = os.path.join(self._config.log_root, 'train')
         vocab_metadata_path = os.path.join(train_dir, 'vocab_metadata.tsv')
         self._vocab.write_metadata(vocab_metadata_path)  # write metadata file
         summary_writer = tf.summary.FileWriter(train_dir)
@@ -531,7 +541,7 @@ class SummarizationModel(object):
 
             # For pointer-generator model, calc final distribution from copy distribution
             # and vocabulary distribution
-            if FLAGS.pointer_gen:
+            if config.pointer_gen:
                 final_dists = self._calc_final_dist(vocab_dists, self.attn_dists)
             else:  # final distribution is just vocabulary distribution
                 final_dists = vocab_dists
@@ -539,7 +549,7 @@ class SummarizationModel(object):
             if config.mode in ['train', 'eval']:
                 # Calculate loss
                 with tf.variable_scope('loss'):
-                    if FLAGS.pointer_gen:
+                    if config.pointer_gen:
                         # Calculate loss per step
                         # This is fiddly; we use tf.gather_nd to pick out the probabilities of the gold target words
                         loss_per_step = []  # will be list length max_dec_steps containing shape (batch_size)
@@ -598,7 +608,8 @@ class SummarizationModel(object):
         tf.summary.scalar('global_norm', global_norm)
 
         # Apply adagrad optimizer
-        optimizer = tf.train.AdagradOptimizer(self._config.lr, initial_accumulator_value=self._config.adagrad_init_acc)
+        optimizer = tf.train.AdagradOptimizer(self._config.learning_rate,
+                                              initial_accumulator_value=self._config.adagrad_init_acc)
         with tf.device('/gpu:0'):
             self._train_op = optimizer.apply_gradients(zip(grads, tvars),
                                                        global_step=self.global_step, name='train_step')
@@ -726,7 +737,7 @@ class SummarizationModel(object):
             'states': self._dec_out_state,
             'attn_dists': self.attn_dists
         }
-        if FLAGS.pointer_gen:
+        if self._config.pointer_gen:
             feed_dict[self._enc_batch_extend_vocab] = batch.enc_batch_extend_vocab,
             feed_dict[self._max_art_oovs] = batch.max_art_oovs,
             to_return['p_gens'] = self.p_gens
@@ -745,7 +756,7 @@ class SummarizationModel(object):
         assert len(results['attn_dists']) == 1
         attn_dists = results['attn_dists'][0].tolist()
 
-        if FLAGS.pointer_gen:
+        if self._config.pointer_gen:
             # Convert singleton list containing a tensor to a list of k arrays
             assert len(results['p_gens']) == 1
             p_gens = results['p_gens'][0].tolist()
@@ -753,7 +764,7 @@ class SummarizationModel(object):
             p_gens = [None for _ in range(beam_size)]
 
         # Convert the coverage tensor to a list length k containing the coverage vector for each hypothesis
-        if FLAGS.coverage:
+        if self._config.coverage:
             new_coverage = results['coverage'].tolist()
             assert len(new_coverage) == beam_size
         else:

src/tf_model/pointer_generator/util.py
@@ -1,14 +1,14 @@
 """ This file contains code to process data into batches """
-import tf_model.pointer_generator.data as data
 import numpy as np
 import os
 from queue import Queue
 from random import shuffle
 import tensorflow as tf
+from tensorflow.python import debug as tf_debug
+from tf_model.pointer_generator.data import abstract2ids, abstract2sents, article2ids, example_generator
+from tf_model.pointer_generator.data import PAD_TOKEN, START_DECODING, STOP_DECODING
 from threading import Thread
 import time
-
-FLAGS = tf.app.flags.FLAGS
 
 
 class Batch(object):
@@ -22,11 +22,6 @@ class Batch(object):
         :param config: hyperparameters
         :param vocab: Vocabulary object
         """
-        self.pad_id = vocab.word2id(data.PAD_TOKEN)  # id of the PAD token used to pad sequences
-        self.init_encoder_seq(example_list, config)  # initialize the input to the encoder
-        self.init_decoder_seq(example_list, config)  # initialize the input and targets for the decoder
-        self.store_orig_strings(example_list)  # store the original strings
-
         self.enc_batch = None
         self.enc_lens = None
         self.enc_padding_mask = None
@@ -39,6 +34,10 @@ class Batch(object):
         self.original_articles = []
         self.original_abstracts = []
         self.original_abstracts_sents = []
+        self.pad_id = vocab.word2id(PAD_TOKEN)  # id of the PAD token used to pad sequences
+        self.init_encoder_seq(example_list, config)  # initialize the input to the encoder
+        self.init_decoder_seq(example_list, config)  # initialize the input and targets for the decoder
+        self.store_orig_strings(example_list)  # store the original strings
 
     def init_encoder_seq(self, example_list, config):
         """
@@ -217,7 +216,7 @@ class Batcher(object):
         """
         # If the batch queue is empty, print a warning
         if self._batch_queue.qsize() == 0:
-            tf.logging.warning('Bucket input queue is empty when calling next_batch. ' +
+            tf.logging.warning('Bucket input queue is empty when calling next_batch. '
                                'Bucket queue size: %i, Input queue size: %i',
                                self._batch_queue.qsize(), self._example_queue.qsize())
             if self._single_pass and self._finished_reading:
@@ -229,7 +228,7 @@ class Batcher(object):
 
     def fill_example_queue(self):
         """ Reads data from file and processes into Examples which are then placed into the example queue. """
-        input_gen = self.text_generator(data.example_generator(self._data_path, self._single_pass))
+        input_gen = self.text_generator(example_generator(self._data_path, self._single_pass))
 
         while True:
             try:
@@ -238,7 +237,7 @@ class Batcher(object):
             except StopIteration:  # if there are no more examples:
                 tf.logging.info('The example generator for this example-queue-filling-thread has exhausted data.')
                 if self._single_pass:
-                    tf.logging.info("single_pass mode is on, so we've finished reading dataset. " +
+                    tf.logging.info("single_pass mode is on, so we've finished reading dataset. "
                                     "This thread is stopping.")
                     self._finished_reading = True
                     break
@@ -246,7 +245,7 @@ class Batcher(object):
                     raise Exception('single_pass mode is off but the example generator is out of data; error.')
 
             # Use the <s> and </s> tags in abstract to get a list of sentences.
-            abstract_sentences = [sent.strip() for sent in data.abstract2sents(abstract)]
+            abstract_sentences = [sent.strip() for sent in abstract2sents(abstract)]
             example = Example(article, abstract_sentences, self._vocab, self._config)  # Process into an Example.
             self._example_queue.put(example)  # place the Example in the example queue.
 
@@ -306,21 +305,21 @@ class Batcher(object):
                     new_t.start()
 
     # noinspection PyMethodMayBeStatic
-    def text_generator(self, example_generator):
+    def text_generator(self, example_gen):
         """
         Generates article and abstract text from tf.Example.
 
-        :param example_generator: a generator of tf.Examples from file. See data.example_generator
+        :param example_gen: a generator of tf.Examples from file. See data.example_generator
         :return:
         """
         while True:
-            e = next(example_generator)  # e is a tf.Example
+            eg = next(example_gen)  # e is a tf.Example
             try:
                 # the article text was saved under the key 'article' in the data files
-                article_text = e.features.feature['article'].bytes_list.value[0].decode()
+                article_text = eg.features.feature['article'].bytes_list.value[0].decode()
 
                 # the abstract text was saved under the key 'abstract' in the data files
-                abstract_text = e.features.feature['abstract'].bytes_list.value[0].decode()
+                abstract_text = eg.features.feature['abstract'].bytes_list.value[0].decode()
             except ValueError:
                 tf.logging.error('Failed to get article or abstract from example')
                 continue
@@ -349,8 +348,8 @@ class Example(object):
         self.config = config
 
         # Get ids of special tokens
-        start_decoding = vocab.word2id(data.START_DECODING)
-        stop_decoding = vocab.word2id(data.STOP_DECODING)
+        start_decoding = vocab.word2id(START_DECODING)
+        stop_decoding = vocab.word2id(STOP_DECODING)
 
         # Process the article
         article_words = article.split()
@@ -378,11 +377,11 @@ class Example(object):
         if config.pointer_gen:
             # Store a version of the enc_input where in-article OOVs are represented by their
             # temporary OOV id; also store the in-article OOVs words themselves
-            self.enc_input_extend_vocab, self.article_oovs = data.article2ids(article_words, vocab)
+            self.enc_input_extend_vocab, self.article_oovs = article2ids(article_words, vocab)
 
             # Get a version of the reference summary where in-article OOVs are represented by
             # their temporary article OOV id
-            abs_ids_extend_vocab = data.abstract2ids(abstract_words, vocab, self.article_oovs)
+            abs_ids_extend_vocab = abstract2ids(abstract_words, vocab, self.article_oovs)
 
             # Overwrite decoder target sequence so it uses the temp article OOV ids
             _, self.target = self.get_dec_inp_targ_seqs(abs_ids_extend_vocab, config.max_dec_steps,
@@ -440,6 +439,59 @@ class Example(object):
                 self.enc_input_extend_vocab.append(pad_id)
 
 
+def calc_running_avg_loss(loss, running_avg_loss, summary_writer, step, decay=0.99):
+    """
+    Calculate the running average loss using exponential decay.
+
+    Used to implement early stopping w.r.t. a smoother loss curve than the raw loss curve.
+
+    :param loss: loss on the most recent eval step
+    :param running_avg_loss: running_avg_loss so far
+    :param summary_writer: FileWriter object to write for TensorBoard
+    :param step: training step
+    :param decay: rate of exponential decay, a float between 0 and 1. Larger is smoother.
+    :return:
+        running_avg_loss: new running average loss
+    """
+    if running_avg_loss == 0:
+        # on the first iteration just take the loss
+        running_avg_loss = loss
+    else:
+        running_avg_loss *= decay + (1 - decay) * loss
+
+    running_avg_loss = min(running_avg_loss, 12)  # clip
+    loss_sum = tf.Summary()
+    tag_name = 'running_avg_loss/decay=%f' % decay
+    loss_sum.value.add(tag=tag_name, simple_value=running_avg_loss)
+    summary_writer.add_summary(loss_sum, step)
+    tf.logging.info('running_avg_loss: %f', running_avg_loss)
+    return running_avg_loss
+
+
+def convert_to_coverage_model(log_root):
+    """ Load non-coverage checkpoint, add initialized extra variables for coverage, and save as new checkpoint """
+    tf.logging.info('Converting non-coverage model to coverage model...')
+
+    # initialize an entire coverage model from scratch
+    sess = tf.Session(config=get_config())
+    print('initializing everything...')
+    sess.run(tf.global_variables_initializer())
+
+    # load all non-coverage weights from checkpoint
+    saver = tf.train.Saver([v for v in tf.global_variables() if 'coverage' not in v.name and 'Adagrad' not in v.name])
+    print('restoring non-coverage variables...')
+    curr_ckpt = load_ckpt(saver, sess, log_root)
+    print('Restored!')
+
+    # save this model and quit
+    new_filename = curr_ckpt + '_cov_init'
+    print('Saving model to %s...' % new_filename)
+    new_saver = tf.train.Saver()  # this one will save all variables that now exist
+    new_saver.save(sess, new_filename)
+    print('Saved!')
+    exit()
+
+
 def get_config():
     """ Returns config for tf.session """
     config = tf.ConfigProto(allow_soft_placement=True)
@@ -448,20 +500,21 @@ def get_config():
 
 
 # noinspection PyBroadException
-def load_ckpt(saver, sess, ckpt_dir='train'):
+def load_ckpt(saver, sess, log_root, ckpt_dir='train'):
     """
     Load checkpoint from the ckpt_dir (if unspecified, this is train dir) and restore it
     to saver and sess, waiting 10 secs in the case of failure. Also returns checkpoint name.
 
     :param saver:
     :param sess:
+    :param log_root:
     :param ckpt_dir:
     :return:
     """
     while True:
         try:
             latest_filename = 'checkpoint_best' if ckpt_dir == 'eval' else None
-            ckpt_dir = os.path.join(FLAGS.log_root, ckpt_dir)
+            ckpt_dir = os.path.join(log_root, ckpt_dir)
             ckpt_state = tf.train.get_checkpoint_state(ckpt_dir, latest_filename=latest_filename)
             tf.logging.info('Loading checkpoint %s', ckpt_state.model_checkpoint_path)
             saver.restore(sess, ckpt_state.model_checkpoint_path)
@@ -469,3 +522,153 @@ def load_ckpt(saver, sess, ckpt_dir='train'):
         except Exception:
             tf.logging.info('Failed to load checkpoint from %s. Sleeping for %i secs...', ckpt_dir, 10)
             time.sleep(10)
+
+
+def restore_best_model(log_root):
+    """ Load best model file from eval directory, add variables for adagrad, and save to train directory """
+    tf.logging.info('Restoring best model for training...')
+
+    # Initialize all vars in the model
+    sess = tf.Session(config=get_config())
+    print('Initializing all variables...')
+    sess.run(tf.initialize_all_variables())
+
+    # Restore the best model from eval dir
+    saver = tf.train.Saver([v for v in tf.all_variables() if 'Adagrad' not in v.name])
+    print('Restoring all non-adagrad variables from best model in eval dir...')
+    curr_ckpt = load_ckpt(saver, sess, log_root, 'eval')
+    print('Restored %s.' % curr_ckpt)
+
+    # Save this model to train dir and quit
+    new_model_name = curr_ckpt.split('/')[-1].replace('bestmodel', 'model')
+    new_filename = os.path.join(log_root, 'train', new_model_name)
+    print('Saving model to %s...' % new_filename)
+    new_saver = tf.train.Saver()  # this saver saves all variables that now exist, including the Adagrad variables
+    new_saver.save(sess, new_filename)
+    print('Saved!')
+    exit()
+
+
+def run_eval(model, batcher, constants):
+    """
+    Repeatedly runs eval iterations, logging to screen and writing summaries.
+    Saves the model with the best loss seen so far.
+
+    :param model:
+    :param batcher:
+    :param constants:
+    :return:
+    """
+    model.build_graph()
+    saver = tf.train.Saver(max_to_keep=3)  # keep 3 best checkpoints at a time
+    sess = tf.Session(config=get_config())
+    eval_dir = os.path.join(constants['log_root'], 'eval')  # make a subdir of the root dir for eval data
+    best_model_save_path = os.path.join(eval_dir, 'bestmodel')  # this is where checkpoints of best models are saved
+    summary_writer = tf.summary.FileWriter(eval_dir)
+
+    # the eval job keeps a smoother running average loss to tell it when to implement early stopping
+    running_avg_loss = 0
+    best_loss = None  # hold the best loss achieved so far
+    while True:
+        load_ckpt(saver, sess, constants['log_root'])  # load a new checkpoint
+        batch = batcher.next_batch()
+
+        # run eval on the batch
+        t0 = time.time()
+        results = model.run_eval_step(sess, batch)
+        t1 = time.time()
+        tf.logging.info('seconds for batch: %.2f', t1 - t0)
+
+        # print the loss and coverage loss to screen
+        loss = results['loss']
+        tf.logging.info('loss: %f', loss)
+        if constants['coverage']:
+            coverage_loss = results['coverage_loss']
+            tf.logging.info('coverage_loss: %f', coverage_loss)
+
+        # add summaries
+        summaries = results['summaries']
+        train_step = results['global_step']
+        summary_writer.add_summary(summaries, train_step)
+
+        # calculate running avg loss
+        running_avg_loss = calc_running_avg_loss(np.asscalar(loss), running_avg_loss, summary_writer, train_step)
+
+        # If running_avg_loss is best so far, save this checkpoint (early stopping).
+        # These checkpoints will appear as bestmodel-<iteration_number> in the eval dir.
+        if best_loss is None or running_avg_loss < best_loss:
+            tf.logging.info('Found new best model with %.3f running_avg_loss. Saving to %s',
+                            running_avg_loss, best_model_save_path)
+            saver.save(sess, best_model_save_path, global_step=train_step, latest_filename='checkpoint_best')
+            best_loss = running_avg_loss
+
+        # flush the summary writer every so often
+        if train_step % 100 == 0:
+            summary_writer.flush()
+
+
+def run_training(model, batcher, sess_context_manager, summary_writer, constants):
+    """ Repeatedly runs training iterations, logging loss to screen and writing summaries """
+    tf.logging.info('Starting run_training...')
+    with sess_context_manager as sess:
+        if constants['debug']:
+            sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+            sess.add_tensor_filter('has_inf_or_nan', tf_debug.has_inf_or_nan)
+
+        while True:  # repeats until interrupted
+            batch = batcher.next_batch()
+            tf.logging.info('running training step...')
+            t0 = time.time()
+            results = model.run_train_step(sess, batch)
+            t1 = time.time()
+            tf.logging.info('seconds for training step: %.3f', t1 - t0)
+            loss = results['loss']
+            tf.logging.info('loss: %f', loss)  # print the loss to screen
+            if not np.isfinite(loss):
+                raise Exception('Loss is not finite. Stopping.')
+
+            if constants['coverage']:
+                coverage_loss = results['coverage_loss']
+                tf.logging.info('coverage_loss: %f', coverage_loss)  # print the coverage loss to screen
+
+            # get the summaries and iteration number so we can write summaries to TensorBoard
+            summaries = results['summaries']  # write these summaries to TensorBoard using summary_writer
+            train_step = results['global_step']  # we need this to update our running average loss
+            summary_writer.add_summary(summaries, train_step)  # write the summaries
+            if train_step % 100 == 0:  # flush the summary writer every so often
+                summary_writer.flush()
+
+
+def setup_training(model, batcher, constants):
+    """ Setup before starting training """
+    train_dir = os.path.join(constants['log_root'], 'train')
+    if not os.path.exists(train_dir):
+        os.makedirs(train_dir)
+
+    model.build_graph()
+    if constants['convert_to_coverage_model']:
+        assert constants['coverage'], 'To convert your non-coverage model to a coverage model, ' \
+                                      'run with convert_to_coverage_model=True and coverage=True'
+        convert_to_coverage_model(constants['log_root'])
+
+    if constants['restore_best_model']:
+        restore_best_model(constants['log_root'])
+
+    saver = tf.train.Saver(max_to_keep=3)  # keep 3 checkpoints at a time
+    supervisor = tf.train.Supervisor(logdir=train_dir,
+                                     is_chief=True,
+                                     saver=saver,
+                                     summary_op=None,
+                                     save_summaries_secs=60,  # save summaries for TensorBoard every 60 secs
+                                     save_model_secs=60,      # checkpoint every 60 secs
+                                     global_step=model.global_step)
+    summary_writer = supervisor.summary_writer
+    tf.logging.info('Preparing or waiting for session...')
+    sess_context_manager = supervisor.prepare_or_wait_for_session(config=get_config())
+    tf.logging.info('Created session!')
+    try:
+        # this is an infinite loop until interrupted
+        run_training(model, batcher, sess_context_manager, summary_writer, constants)
+    except KeyboardInterrupt:
+        tf.logging.info('Caught keyboard interrupt on worker. Stopping supervisor...')
+        supervisor.stop()